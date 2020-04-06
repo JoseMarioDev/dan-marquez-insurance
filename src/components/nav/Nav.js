ac5b241008933a861b27390403f9de16af3e1f7c
josemariodev <josemariodev@gmail.com>
2020-04-06 01:04:29 -0500

    :lipstick: Styling Nav
    Nav styling and structure WIP

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,14 +1,24 @@
 // left side - logo w/ a tags to link back home
 // middle - links in middle of navbar
 // right - social link and phone
+
 import React from 'react';
 import Logo from '../../assets/img/logo1.svg';
 
 const Nav = () => (
-  <div>
+  <div className="navContainer">
+    {/* left side logo */}
     <a href="/home">
-      <img src={Logo} alt="logo" />
+      <img src={Logo} alt="logo" className="navLogo" />
     </a>
+    {/* middle section nav links */}
+    <ul className="navLinks">
+      <a href="/about" className="navLink">
+        About
+      </a>
+    </ul>
+    {/* right side, socials and phone */}
+    <div className="socials">socials here</div>
   </div>
 );
 
